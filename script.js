4bb85593874c08845ead14ccdaf233ffe08111b7
saro <143511143+sarobabi@users.noreply.github.com>
2024-01-24 15:38:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("button cliked")
+    formValidation();
+});
+
+
+let formValidation = () => {
+    if (input.value === "") {
+        msg.innerHTML = "post cannot be blank"
+        console.log("failure")
+    }
+    else {
+        console.log("success")
+        msg.innerHTML = "";
+        acceptData();
+
+    }
+};
+
+let data = {};
+
+let acceptData = () => {
+    data["test"] = input.value;
+    console.log(data)
+    createPost();
+};
+
+let createPost = () => {
+    posts.innerHTML += `
+        <div>
+            <p>${data.test}</p>
+            <span class="options">
+                <i onClick="editPost(this)" class="fa-solid fa-pen-to-square"></i>
+                <i onClick="deletePost(this)" class="fa-solid fa-trash"></i>
+            </span>
+        </div>
+    `;
+    input.value = "";
+};
+
+let deletePost = (e) => {
+    e.parentElement.parentElement.remove();
+};
+
+let editPost=(e)=>{
+    input.value=e.parentElement.previousElementSibling.innerHTML;
+    e.parentElement.parentElement.remove();
+};
